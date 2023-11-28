5c86395b780893dfb18bf6f5a85ebb2d7f765840
JSun <65435933+saldandpaper@users.noreply.github.com>
2023-11-27 23:05:25 -0700

    add hover button

--- scriptLib/globalScript.js
+++ scriptLib/globalScript.js
@@ -1,8 +1,31 @@
+//alert button
 const buttonAlert = () => {
     alert("Welcome to my portfolio. ")
 }
 
+//Hover Button method 1 (refactored)
+const hoverButton = document.getElementById("btn-alert");
+hoverButton.onmouseover = () => {
+    hoverButton.innerText = "Hovering...";
+}
+hoverButton.onmouseleave = () => {
+    hoverButton.innerText = "Alert Button";
+}
 
+//Hover Button method 2 with eventListeners
+// const hoverText = document.getElementById("btn-alert");
+// hoverText.addEventListener(
+//     "mouseover", 
+//     (event) => {
+//         event.target.innerText = "hovering...";
+//     }, false
+// );
+// hoverText.addEventListener(
+//     "mouseout",
+//     (event) => {
+//         event.target.innerText = "Alert Button";
+//     }, false,
+// )
 
-
+//footer with dynamic year display
 document.getElementById("footerYear").innerHTML = new Date().getFullYear();
